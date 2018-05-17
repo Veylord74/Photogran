8a9b2015e597efe05c092e9b24757e67729921a3
Thibaut Martinot <thibaut.m@hotmail.fr>
2018-05-18 00:17:49 +0200

    Fin du téléchargement du JSON et début de l'adapter et RecyclerView

--- app/src/main/java/thibaut/photogran/GetPhotosService.java
+++ app/src/main/java/thibaut/photogran/GetPhotosService.java
@@ -3,10 +3,22 @@ package thibaut.photogran;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class GetPhotosService extends IntentService {
-    private static final String ACTION_GET_PHOTOS = "thibaut.photogran.action.FOO";
+    private static final String ACTION_GET_PHOTOS = "thibaut.photogran.action.GET_PHOTOS";
 
     public GetPhotosService() {
         super("GetPhotosService");
@@ -30,5 +42,37 @@ public class GetPhotosService extends IntentService {
 
     private void handleActionGetPhotos() {
         Log.d("GetPhotosService", "Thread service name:" + Thread.currentThread().getName());
+        URL url = null;
+        try {
+            url = new URL("https://api.unsplash.com/photos/?client_id=f81da655d58e3b1a931d10d89fa6c7a40f3b8d0efa4b5b2a4e0578c17729bdaa");
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            if(HttpURLConnection.HTTP_OK == conn.getResponseCode()) {
+                copyInputStreamToFile(conn.getInputStream(),
+                        new File(getCacheDir(), "photos.json"));
+                Log.d("Service", "photos.json downloading !");
+                LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(SecondeActivity.PHOTOS_UPDATE));
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void copyInputStreamToFile(InputStream in, File file) {
+        try {
+            OutputStream out = new FileOutputStream(file);
+            byte[] buf = new byte[1024];
+            int len;
+            while((len=in.read(buf))>0) {
+                out.write(buf,0,len);
+            }
+            out.close();
+            in.close();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/thibaut/photogran/SecondeActivity.java
+++ app/src/main/java/thibaut/photogran/SecondeActivity.java
@@ -1,11 +1,31 @@
 package thibaut.photogran;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class SecondeActivity extends AppCompatActivity {
+
+    RecyclerView rv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +38,75 @@ public class SecondeActivity extends AppCompatActivity {
                 GetPhotosService.startActionGetPhotos(SecondeActivity.this);
             }
         });
+        IntentFilter intentFilter = new IntentFilter(PHOTOS_UPDATE);
+        LocalBroadcastManager.getInstance(this).registerReceiver(new PhotoUpdate(),intentFilter);
+
+        rv = findViewById(R.id.rv_photos);
+        rv.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+    }
+
+    public static final String PHOTOS_UPDATE = "thibaut.photogran.PHOTOS_UPDATE";
+
+    public class PhotoUpdate extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            /*rv = (RecyclerView) findViewById(R.id.rv_poke);
+            BiersAdapter ba = (BiersAdapter) rv.getAdapter();
+            ba.setNewBier(getPhotosFromFile());*/
+            Log.d("SecondActivity", "Successfuly dowloaded !");
+        }
+    }
+
+    public JSONArray getPhotosFromFile(){
+        try{
+            InputStream is = new FileInputStream(getCacheDir() + "/" + "photos.json");
+            byte[] buffer = new byte[is.available()];
+            is.read(buffer);
+            is.close();
+            return new JSONObject(new String(buffer, "UTF-8")).getJSONArray("objects");
+        } catch(IOException e){
+            e.printStackTrace();
+            return new JSONArray();
+        } catch(JSONException e){
+            e.printStackTrace();
+            return new JSONArray();
+        }
+    }
+
+    class PhotosAdapter extends RecyclerView.Adapter<PhotosAdapter.PhotoHolder> {
+
+        private JSONArray photos;
+
+        @Override
+        public PhotoHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(PhotoHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        public class PhotoHolder extends RecyclerView.ViewHolder {
+
+            public ImageView photo;
+
+            public PhotoHolder(View view) {
+
+                super(view);
+                photo = view.findViewById(R.id.rv_photo_element);
+
+            }
+
+            public void display(String url) {
+                //photo.setText(url);
+            }
+        }
+
     }
 }
